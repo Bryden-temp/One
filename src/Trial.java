890a02063164991c9a8d0c30f58e473934f133b0
willb8313 <willb8313@FHC-IC0037269.wrdsb.ca>
2015-04-07 13:46:45 -0400

    s

---
+++ src/Trial.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author willb8313
+ */
+public class Trial {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
